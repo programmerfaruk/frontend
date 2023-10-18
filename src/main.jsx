fd13fb5a2e72b02d3acca6508f352bd32697e7d6
--delete <--delete>
2023-10-18 18:51:51 +0600

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { myRoute } from './Route/router'
+import AuthProvider from './Providers/AuthProvider'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthProvider>
+      <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
+      <RouterProvider router={myRoute}></RouterProvider>
+    </AuthProvider>  </React.StrictMode>,
 )
